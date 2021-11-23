6bf9b6a3e8220b5596e42b6555ce20a27de19f54
Jack <js02220@surrey.ac.uk>
2021-11-23 19:44:07 +0000

    changed header and footer to haml files, added devise and some comments

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,6 @@ class Artist < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   #validates :active, presence: true
+  #commented out above as was not saving artists if active value was set to false.
   has_many :albums, dependent: :destroy
 end
