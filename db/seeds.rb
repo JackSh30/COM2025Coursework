7ede0e98c1095e4bfd798af9f79f0e74dc2d86b1
Jack <js02220@surrey.ac.uk>
2021-11-26 16:35:39 +0000

    fixed album seeds and added song seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ whiteAlbum.delete if whiteAlbum
 albums = Album.create([title: 'The Beatles',
                         artist_id: 1,
                         description: 'The greatest album of all time',
-                        release: 1968-11-22,
+                        release: "1968-11-22",
                         numOfSongs: 30,
                         length: 93.33])
 
@@ -57,7 +57,7 @@ exile.delete if exile
 albums = Album.create([title: 'Exile on Main St.',
                         artist_id: 2,
                         description: 'Largely recorded in the South of France',
-                        release: 1972-5-12,
+                        release: "1972-5-12",
                         numOfSongs: 18,
                         length: 67.07])
 
@@ -67,7 +67,7 @@ innervisions.delete if innervisions
 albums = Album.create([title: 'Innervisions',
                         artist_id: 3,
                         description: 'The pinnacle of psychedelic soul',
-                        release: 1973-8-3,
+                        release: "1973-8-3",
                         numOfSongs: 9,
                         length: 44.15])
 
@@ -77,7 +77,7 @@ lovedAMan.delete if lovedAMan
 albums = Album.create([title: 'I Never Loved a Man the Way I Love You',
                         artist_id: 4,
                         description: 'Includes her hit cover of Otis Redding\'s Respect',
-                        release: 1967-3-10,
+                        release: "1967-3-10",
                         numOfSongs: 11,
                         length: 32.51])
 
@@ -87,6 +87,41 @@ blonde.delete if blonde
 albums = Album.create([title: 'Blonde on Blonde',
                         artist_id: 5,
                         description: 'The third album in Dylan\'s electric trilogy',
-                        release: 1966-6-20,
+                        release: "1966-6-20",
                         numOfSongs: 14,
                         length: 72.57])
+
+gentlyWeep = Song.where(title: 'While My Guitar Gently Weeps').first
+gentlyWeep.delete if gentlyWeep
+
+songs = Song.create([title: 'While My Guitar Gently Weeps',
+                      album_id: 1,
+                      length: 4.46])
+
+happy = Song.where(title: 'Happy').first
+happy.delete if happy
+
+songs = Song.create([title: 'Happy',
+                      album_id: 2,
+                      length: 3.04])
+
+city = Song.where(title: 'Living for the City').first
+city.delete if city
+
+songs = Song.create([title: 'Living for the City',
+                      album_id: 3,
+                      length: 7.03])
+
+respect = Song.where(title: 'Respect').first
+respect.delete if respect
+
+songs = Song.create([title: 'Respect',
+                      album_id: 4,
+                      length: 2.27])
+
+johanna = Song.where(title: 'Visions of Johanna').first
+johanna.delete if johanna
+
+songs = Song.create([title: 'Visions of Johanna',
+                      album_id: 5,
+                      length: 7.33])
